9a82beb75b9630e14834855d69a40211419407de
Vemula Sreyamsi <129597472+sreyamsi@users.noreply.github.com>
2023-10-23 00:04:17 +0530

    Add files via upload

---
+++ Port/script.js
@@ -0,0 +1,16 @@
+// let sections=document.querySelectorAll('section');
+// let snavLinks=document.querySelectorAll('home nav a');
+// window.onscroll=()=>{
+//     sections.forEach(sec=>{
+//         let top=window.scrollY;
+//         let offset=sec.offsetTop;
+//         let height=sec.offsetHeight;
+//         let id=sec.getAttribute('id');
+//         if(top>=offset && top<offset+height){
+//             navLinks.forEach(links=>{
+//                 links.classList.remove('active');
+//                 document.querySelector('home nav a[href*='+id+']').classList.add('active');
+//             })
+//         }
+//     })
+// }
